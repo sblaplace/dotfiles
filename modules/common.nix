330b9a81e137fe11ad35fbf02945eee9ea14fd1e
Sarah Laplace <sblaplace@gmail.com>
2026-05-03 01:56:37 -0500

    fix copy fail vuln protection

--- modules/common.nix
+++ modules/common.nix
@@ -39,6 +39,14 @@
 
   boot.blacklistedKernelModules = [ "af_alg" "algif_hash" "algif_skcipher" "algif_rng" "algif_aead" ];
 
+  boot.extraModprobeConfig = ''
+    install af_alg /bin/false
+    install algif_aead /bin/false
+    install algif_hash /bin/false
+    install algif_skcipher /bin/false
+    install algif_rng /bin/false
+  '';
+
   # SSH access
   services.openssh = {
     enable = true;
